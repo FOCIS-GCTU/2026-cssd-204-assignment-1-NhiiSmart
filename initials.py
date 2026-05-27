74b4b888c6e220833b09d8c43de8c51b66949374
Caleb Nii Adjetey Laryea <2425402475@live.gctu.edu.gh>
2026-05-27 08:23:33 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -11,25 +11,20 @@
 # and I have not provided this code to any other student.
 
 
-def main():
-    # Prints my initials in large stylized letters.
 
-    print()
-    print("...CANL")
-    print()
+print()
+print("...CANL")
+print()
 
-    print("...CCCCCCCCCCCC........AAAAAAAAAAAA........NN........NN........LL...............")
-    print("...CC..................AAA......AAA........NNN.......NN........LL...............")
-    print("...CC..................AAA......AAA........NNNN......NN........LL...............")
-    print("...CC..................AAAAAAAAAAAA........NN.NN.....NN........LL...............")
-    print("...CC..................AAAAAAAAAAAA........NN..NN....NN........LL...............")
-    print("...CC..................AAA......AAA........NN...NN...NN........LL...............")
-    print("...CC..................AAA......AAA........NN....NN..NN........LL...............")
-    print("...CC..................AAA......AAA........NN.....NN.NN........LL...............")
-    print("...CC.............**...AAA......AAA...**...NN......NNNN...**...LLLLLLLLLLLL...**")
-    print("...CCCCCCCCCCCC...**...AAA......AAA...**...NN.......NNN...**...LLLLLLLLLLLL...**")
+print("...CCCCCCCCCCCC........AAAAAAAAAAAA........NN........NN........LL...............")
+print("...CC..................AAA......AAA........NNN.......NN........LL...............")
+print("...CC..................AAA......AAA........NNNN......NN........LL...............")
+print("...CC..................AAAAAAAAAAAA........NN.NN.....NN........LL...............")
+print("...CC..................AAAAAAAAAAAA........NN..NN....NN........LL...............")
+print("...CC..................AAA......AAA........NN...NN...NN........LL...............")
+print("...CC..................AAA......AAA........NN....NN..NN........LL...............")
+print("...CC..................AAA......AAA........NN.....NN.NN........LL...............")
+print("...CC.............**...AAA......AAA...**...NN......NNNN...**...LLLLLLLLLLLL...**")
+print("...CCCCCCCCCCCC...**...AAA......AAA...**...NN.......NNN...**...LLLLLLLLLLLL...**")
 
-    print()
-
-
-main()
+print()
