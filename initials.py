3f299335ebf8a563c0bc08e2dd798c94f3e37596
Caleb Nii Adjetey Laryea <2425402475@live.gctu.edu.gh>
2026-05-27 03:46:05 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 2
+#
+# Name: Laryea Caleb Nii Adjetey
+# STUDENT ID: 242524075
+# Email: kayleblaryea@gmail.com
+# Grader: Augustus
+#
+# On my honor, Laryea Caleb Nii Adjetey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters.
 
+    print()
+    print("...CANL")
+    print()
+
+    print("...CCCCCCCCCCCC........AAAAAAAAAAAA........NN........NN........LL...............")
+    print("...CC..................AAA......AAA........NNN.......NN........LL...............")
+    print("...CC..................AAA......AAA........NNNN......NN........LL...............")
+    print("...CC..................AAAAAAAAAAAA........NN.NN.....NN........LL...............")
+    print("...CC..................AAAAAAAAAAAA........NN..NN....NN........LL...............")
+    print("...CC..................AAA......AAA........NN...NN...NN........LL...............")
+    print("...CC..................AAA......AAA........NN....NN..NN........LL...............")
+    print("...CC..................AAA......AAA........NN.....NN.NN........LL...............")
+    print("...CC.............**...AAA......AAA...**...NN......NNNN...**...LLLLLLLLLLLL...**")
+    print("...CCCCCCCCCCCC...**...AAA......AAA...**...NN.......NNN...**...LLLLLLLLLLLL...**")
+
+    print()
 
 
 main()
